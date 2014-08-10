cfe013d4dddc019ea3d015dda3f5bf113b560334
Robert <robertzwinchell@gmail.com>
2014-08-10 13:47:31 +0200

    search functionality, tooltip functionality

--- nwstooltip/models.py
+++ nwstooltip/models.py
@@ -10,21 +10,26 @@ from invenio.modules.accounts.models import User
 from invenio.modules.baskets.models import BskBASKET
 from invenio.modules.search.models import WebQuery
 
+
+
+
 class NwsToolTip(db.Model):
     """Represents a NwsToolTip record."""
     __tablename__ = 'nwsTOOLTIP'
-    id = db.Column(db.Integer(15, unsigned=True), nullable=False,
-                server_default='0', primary_key=True)
-    id_story = db.Column(db.Integer(15, unsigned=True), nullable=False,
-                server_default='0')
-    body = db.Column(db.String(512), nullable=False,
-            server_default='0')
-    target_element = db.Column(db.String(256), nullable=False,
-            server_default='0')
-    target_page = db.Column(db.String(256), nullable=False,
-            server_default='0')
-    nwsTOOLTIPcol = db.Column(db.String(45), nullable=False,
-            server_default='0')
+    id = db.Column(db.Integer(15, unsigned=True), nullable=False, server_default='0', primary_key=True,autoincrement=True)
+    id_story = db.Column(db.Integer(15, unsigned=True), db.ForeignKey('nwsSTORY.id'))
+    body = db.Column(db.String(512), nullable=False, server_default='0')
+    target_element = db.Column(db.String(256), nullable=False,server_default='0')
+    target_page = db.Column(db.String(256), nullable=False)
+
+class NwsSTORY(db.Model):
+    """Represents a nwsSTORY record."""
+    __tablename__ = 'nwsSTORY'
+    id = db.Column(db.Integer(15, unsigned=True), nullable=False, primary_key=True,autoincrement=True)
+    title = db.Column(db.String(256), nullable=False, default='')
+    body = db.Column(db.Text, nullable=False, default='')
+    created = db.Column(db.TIMESTAMP, nullable=False, server_default='9999-12-31 23:59:59')
+    nwsToolTip = db.relationship('NwsToolTip',uselist=False, backref='nwsSTORY' )
 
 
 #def init_db(app):
--- nwstooltip/views.py
+++ nwstooltip/views.py
@@ -2,8 +2,9 @@
 
 """nwsToolTip Flask Blueprint"""
 from flask import Flask
+from flask.ext.login import login_required
 from flask import request, url_for, g, Markup, redirect, flash,Blueprint,render_template
-from . models import  NwsToolTip
+from . models import  NwsToolTip,NwsSTORY
 
 from sqlalchemy.exc import IntegrityError
 from werkzeug.debug import DebuggedApplication
@@ -15,4 +16,12 @@ blueprint = Blueprint('nwsToolTip', __name__, template_folder='templates',static
 def index():
     tooltips = NwsToolTip.query.all()
     return render_template('index.html',tooltips=tooltips)
+@blueprint.route('/admin/search', methods=['GET', 'POST'])
+def search_index():
+    return render_template('search.html')
+@blueprint.route('/do_search', methods=['GET', 'POST'])
+def do_search():
+	 if request.method == 'POST':
+		result = NwsSTORY.query.filter(NwsSTORY.title.contains(request.form['keywords']) | NwsSTORY.body.contains(request.form['keywords'])).all()
+		return render_template('result.html',searchResult=result)
 
