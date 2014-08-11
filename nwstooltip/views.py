47932cb3366a7bec828ef20018de9a6c58527607
Robert <robertzwinchell@gmail.com>
2014-08-11 23:02:25 +0200

    admin page started, search fixed

--- nwstooltip/views.py
+++ nwstooltip/views.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask.ext.login import login_required
 from flask import request, url_for, g, Markup, redirect, flash,Blueprint,render_template
 from . models import  NwsToolTip,NwsSTORY
+from sqlalchemy.exc import IntegrityError
 
 from sqlalchemy.exc import IntegrityError
 from werkzeug.debug import DebuggedApplication
@@ -24,4 +25,11 @@ def do_search():
 	 if request.method == 'POST':
 		result = NwsSTORY.query.filter(NwsSTORY.title.contains(request.form['keywords']) | NwsSTORY.body.contains(request.form['keywords'])).all()
 		return render_template('result.html',searchResult=result)
+
+@blueprint.route('/admin/news', methods=['GET', 'POST'])
+def admin():
+    #Return the form and the messages so far
+    nwsTOOLTIP = NwsToolTip.query.all()
+    return render_template('admin.html', tooltips=nwsTOOLTIP)
+
 
